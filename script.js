04cd7497da55d852f8642e532877667a59d1ee84
TKapilesh <kapileshkapilesh1977@gmail.com>
2024-06-17 15:28:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// You can add JavaScript functionality here if needed
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Perform form submission handling here (e.g., AJAX request)
+    // Example: alert('Form submitted!');
+    // Replace alert with actual form submission code
+  });
+  
